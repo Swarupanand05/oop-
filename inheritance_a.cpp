eaad46fdeaf25172035f3b339773d9e7199a0f8d
Swarupanand Popat Bajbalkar <151636990+Swarupanand05@users.noreply.github.com>
2025-02-10 11:07:19 +0530

    Add files via upload
    single inheritance in c+++

---
+++ inheritance_a.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class A
+	{
+	public:
+		int a,b;
+		void get()
+		{
+			cout<<"\nEnter two numbers :";
+			cin>>a>>b;
+		}
+	};
+class B:public A
+{
+	public:
+		void sum()
+		{
+			cout<<"\nAddition :"<<a+b;
+		}
+};
+int main()
+{
+	B obj;
+	obj.get();
+	obj.sum();
+	return 0;
+}
